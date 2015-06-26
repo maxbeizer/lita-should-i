57cbcaa58a463d4881fa87f738f87ef18a1bcf99
Max Beizer <max.beizer@gmail.com>
2015-06-26 17:37:54 -0500

    Add the most basic functionality

--- lib/lita/handlers/should_i.rb
+++ lib/lita/handlers/should_i.rb
@@ -1,7 +1,21 @@
 module Lita
   module Handlers
     class ShouldI < Handler
+      route(/\Ashould I (.+) or (.+)\?\z/, :should_i, command: true, help: {
+        "robot should I _firstthing_ or _lastthing_ ?" => "Robot chooses _firstthing_ or _lastthing_"
+      })
+
+      def should_i(response)
+        option = choose_option response.matches.first
+        response.reply "You should #{option}"
+      end
+
+      private
+      def choose_option(matches)
+        matches.sample
+      end
     end
+
 
     Lita.register_handler(ShouldI)
   end
--- spec/lita/handlers/should_i_spec.rb
+++ spec/lita/handlers/should_i_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe Lita::Handlers::ShouldI, lita_handler: true do
+  it { is_expected.to route_command("should I first or second?").to :should_i }
+
+  it "greets anyone that says hi to it" do
+    send_command "should I first or second?"
+    expect(replies.last).to start_with "You should"
+  end
 end
