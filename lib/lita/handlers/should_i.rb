ce95d9c836d1b72ca86ef631d1c29074f84be0f4
Max Beizer <max.beizer@gmail.com>
2015-06-28 13:04:00 -0500

    Update help instructions

--- lib/lita/handlers/should_i.rb
+++ lib/lita/handlers/should_i.rb
@@ -2,7 +2,7 @@ module Lita
   module Handlers
     class ShouldI < Handler
       route(/\Ashould I (.+) or (.+)\?\z/, :should_i, command: true, help: {
-        "robot should I _firstthing_ or _lastthing_ ?" => "Robot chooses _firstthing_ or _lastthing_"
+        "should I _firstthing_ or _lastthing_ ?" => " Robot chooses _firstthing_ or _lastthing_"
       })
 
       def should_i(response)
